a81f65287e02fbfa3aa1afdc18166816fd6f3d2d
danussh <pdanussh1997@gmail.com>
2021-11-06 16:27:06 +0530

    fix for cors

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(express.json());
 const server = http.createServer(app);
 const io = require("socket.io")(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://danussh.github.io/video-frontend/",
     methods: ["GET", "POST"],
   },
 });
